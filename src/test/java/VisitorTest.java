4f5d166db8913a93f9f1cd38926b293e31c0cf3c
Ifeyinwa Kanu <chika.kanu@procompletions.com>
2019-04-02 20:55:40 +0100

    visitor complete

--- src/test/java/VisitorTest.java
+++ src/test/java/VisitorTest.java
@@ -1,2 +1,34 @@
+import ThemePark.Visitor;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class VisitorTest {
+
+    private Visitor visitor;
+
+    @Before
+    public void before() {
+        visitor = new Visitor(21, 180, 100);
+    }
+
+    @Test
+    public void canGetAge() {
+        visitor.setAge(32);
+        assertEquals(32, visitor.getAge());
+    }
+
+    @Test
+    public void canGetHeight() {
+        visitor.setHeight(176);
+        assertEquals(176, visitor.getHeight());
+    }
+
+    @Test
+    public void hasMoney() {
+        visitor.setMoney(150);
+        assertEquals(150, visitor.getMoney(), 0);
+    }
+
 }
